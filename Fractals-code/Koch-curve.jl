92e7ef5a8e1fa6fcb60ec400b1c746c9168772c5
shahmari <shahmari.acer@outlook.com>
2021-09-01 14:46:07 +0430

    add functions to Koch and combine every versions of that.

--- Fractals-code/Koch-curve.jl
+++ Fractals-code/Koch-curve.jl
@@ -2,13 +2,13 @@ using Plots
 
 Rotation(x, y, θ) = [cos(θ) -sin(θ);sin(θ) cos(θ)] * [x;y]
 
-function KochCurve(; points, order)
+function KochCurve(; points, order, θ)
     for n in 1:order
         i = 1
         while i < length(points)
             insert!(points,i+1,(points[i+1] - points[i]) /3 + points[i])
             delta = (points[i+2] - points[i]) *2 /3 + points[i] - points[i+1]
-            insert!(points, i+2, Rotation(delta[1], delta[2], pi/3)+points[i+1])
+            insert!(points, i+2, Rotation(delta[1], delta[2], θ)+points[i+1])
             insert!(points,i+3,(points[i+3] - points[i]) *2 /3 + points[i])
             i += 4
         end
@@ -16,11 +16,19 @@ function KochCurve(; points, order)
     return points
 end
 
-Parameters = Dict(
-            :points => [[0.0; 0.0],[10.0; 0.0]],
-            :order => 7)
+StartPoints = [[[0.0; 0.0],[10.0; 0.0]],
+            [[0.0; 0.0],[10.0; 0.0],[5.0; 8.66],[0.0; 0.0]],
+            [[0.0; 0.0],[10.0; 0.0],[5.0; 8.66],[0.0; 0.0]],
+            [[0.0; 0.0],[10.0; 0.0],[5.0; 8.66],[0.0; 0.0]]
+                ]
+Adegs = [pi/3, pi/3, -pi/3, 2*pi/3,]
 
-points = KochCurve(;Parameters...)
-
-plot(hcat(points...)[1,:], hcat(points...)[2,:], xlims = (-0.5 ,10), ylims = (-0.5,5), legend = false, border=:none, fill =(0))
-savefig("C:\\Users\\Yaghoub\\Documents\\GitHub\\")
+for i in 1:4
+    Parameters = Dict(
+                :points => StartPoints[i],
+                :order => 5,
+                :θ => Adegs[i])
+    points = KochCurve(;Parameters...)
+    plot(hcat(points...)[1,:], hcat(points...)[2,:], legend = false, border=:none, fill =(0))
+    savefig("C:\\Users\\Yaghoub\\Documents\\GitHub\\A-few-fractals-in-Julia\\Fractals-Fig\\KochCurve$i.png")
+end
