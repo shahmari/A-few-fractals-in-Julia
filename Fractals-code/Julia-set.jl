bf0373174af16d1473e8e865377cac061b7191d8
shahmari <shahmari.acer@outlook.com>
2021-10-01 13:43:06 +0330

    Update Julia-set.jl

--- Fractals-code/Julia-set.jl
+++ Fractals-code/Julia-set.jl
@@ -1,23 +1,30 @@
 using Plots
 
-c = complex(-0.4,0.6)
+cList = [complex(-0.4,-0.6), complex(0,-1),
+            complex(-0.12,-0.75), complex(-0.6,0),
+                complex(-0.8,0.16), complex(-0.4,0.6)]
 
-abst = zeros((601,601))
-inum = 0
-for i in -1.5:0.005:1.5
-    inum += 1
-    jnum = 0
-    for j in -1.5:0.005:1.5
-        jnum += 1
-        z = complex(i,j)
-        for n in 1:70
-            z = (z^2) + c
-            if abs(z) > 1.5
-                abst[inum,jnum] = abs(z)
-                break
+function JuliaSet(c)
+    abst = zeros((601,601))
+    k = (1 + sqrt(1 + (4 * abs(c)))) / 2
+    inum = 0
+    for i in -1.5:0.005:1.5
+        inum += 1
+        jnum = 0
+        for j in -1.5:0.005:1.5
+            jnum += 1
+            z = complex(i,j)
+            for n in 1:70
+                z = (z^2) + c
+                if abs(z) > k
+                    abst[inum,jnum] = abs(z)
+                    break
+                end
             end
         end
     end
+    return abst
 end
+
 theme(:dark)
 heatmap(abst, c = cgrad(:thermal, rev = true), legend = false, border=:none)
